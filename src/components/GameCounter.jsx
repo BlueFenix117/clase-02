27382f3c57d8cf8a001635252b2f3035293a1925
Sebastian Espejel Moran <sebas_esp99@outlook.com>
2024-04-24 18:02:49 -0600

    Se implemento la comprobacion de juegos completados

--- src/components/GameCounter.jsx
+++ src/components/GameCounter.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 export const GameCounter = (props) => {
-  return (
-    <h2 className='GameCounter'>
-        Has Completado <span>{props.completed}</span> de <span>{props.total}</span> juegos
-    </h2>
-  )
+
+  if (props.completed==props.total){
+    return (
+      <h2 className='GameCounter'>
+          Has Completado todos los juegos
+      </h2>
+    )
+  }else
+  {
+    return (
+      <h2 className='GameCounter'>
+          Has Completado <span>{props.completed}</span> de <span>{props.total}</span> juegos
+      </h2>
+    )
+  }
+
+  
 }
 
 export default GameCounter
